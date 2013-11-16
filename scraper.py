c335fb1e08edf94098491e66ed5f5b4f45776945
Nick Collins <nick@vividx.co.uk>
2013-11-16 13:33:50 -0500

    adds support for Sunlight Foundation

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
 import requests
-import login
+from login import keys
 
 ARTICLE_SEARCH_URL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key={key}'
+SUNLIGHT_CONGRESS_URL = 'http://congress.api.sunlightfoundation.com/{method}?apikey={key}'
 
-def get_json(url, key):
-    r = requests.get(url.format(key=key))
-    return r.json()
+def get_json(url):
+    return requests.get(url).json()
 
-print(get_json(ARTICLE_SEARCH_URL, login.keys['nyt_article_search']))
+def sunlight_url(base, method, key):
+    return base.format(method=method, key=key)
+
+print(get_json(sunlight_url(SUNLIGHT_CONGRESS_URL, 'bills', keys['sunlight'])))
