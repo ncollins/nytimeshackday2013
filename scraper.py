c4a14439bdc3689c9c24cee2fd9347ce7def0950
Nick Collins <nick@vividx.co.uk>
2013-11-16 15:47:45 -0500

    `main()` loops over bill pages
    saves text to disk where available

--- scraper.py
+++ scraper.py
@@ -19,23 +19,27 @@ def save_url(url, path, name):
     text = BeautifulSoup(r.text).get_text()
     with open(path + '/' + name + '.txt', 'w') as f:
         f.write(text)
+        print('saved: ' + url)
 
-query = sunlight_query(congress='113', per_page='50')
-#query = ''
-
-url = sunlight_url(SUNLIGHT_CONGRESS_URL, 'bills', query, keys['sunlight'])
-
-bills = get_json(url)
 
 #qHealth = sunlight_query(congress='113', query='health care')
 #bHealth = get_json(sunlight_url(SUNLIGHT_CONGRESS_URL, 'bills/search', qHealth, keys['sunlight']))
 
 def save_bills(bs):
-    for b in enumerate(b['results']):
-        try:
+    for b in bs['results']:
+        number = b['number']
+        title = b['official_title']
+        if b.get('last_version', None):
             link = b['last_version']['urls']['html']
-            number = b['number']
-            title = b['official_title']
             save_url(link, 'bill_text', str(number))
-        except:
-            pass
+
+def main():
+    MAX_PAGES = 500
+    loop = 0
+    while loop < MAX_PAGES:
+        query = sunlight_query(congress='113', per_page='50', page=str(loop))
+        url = sunlight_url(SUNLIGHT_CONGRESS_URL, 'bills', query, keys['sunlight'])
+        bills = get_json(url)
+        save_bills(bills)
+        loop = bills['page']['page'] + 1
+    return loop
